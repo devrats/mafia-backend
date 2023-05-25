3ebb08c4828aa382331190bf31ea8a3e6743bc01
Devvrat Sharma <devvratsharma9759@gmail.com>
2023-05-25 12:07:34 +0530

    implemented chat for mfia

--- index.ts
+++ index.ts
@@ -3,6 +3,24 @@ import { authRoutes } from "./rotes/routes";
 import cors from 'cors'
 import cookieParser from'cookie-parser';
 import dotenv from 'dotenv'
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+io.on('connection', (socket:any) => {
+    console.log('A user connected');
+  
+    socket.on('chat-message', (message:any) => {
+      console.log('Received message:', message);
+      io.emit('chat-message', message);
+      // Handle the received message and broadcast it to other clients
+      // using socket.emit(), io.emit(), or other appropriate methods.
+    });
+  
+    socket.on('disconnect', () => {
+      console.log('A user disconnected');
+      // Handle the disconnection event as needed.
+    });
+  });
+  
 
 const app = express();
 dotenv.config();
@@ -20,3 +38,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log("connected to server succesfully", [PORT]);
 }); 
+
+
+server.listen(3001, () => {
+  console.log('WebSocket server is running on port 3001');
+});
